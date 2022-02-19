9a0d88dabe79ea1761b0f27a8438acbad911eb76
Jorge Pinell <jpinell1075@gmail.com>
2022-02-19 17:07:29 -0600

    Historial Médico Video 2

---
+++ CapaDatos/Repositorios/Repositorio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CapaDatos.Repositorios
+{
+    public abstract class Repositorio
+    {
+        private readonly string connectionString;
+
+        public Repositorio()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["connHistorial"].ToString();
+        }
+
+        protected SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ CapaDatos/Repositorios/RepositorioMaestro.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.Repositorios
+{
+    public abstract class RepositorioMaestro : Repositorio
+    {
+        protected List<SqlParameter> parametros;
+
+        protected int ExecuteNonQuery(string transactSql)
+        {
+            using(var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.Text;
+                    foreach (SqlParameter item in parametros)
+                    {
+                        comando.Parameters.Add(item);
+                    }
+                    int result = comando.ExecuteNonQuery();
+                    parametros.Clear();
+                    return result;
+                }
+            }
+        }
+
+        protected DataTable ExecuteReader(string transactSql)
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.Text;
+                    SqlDataReader reader = comando.ExecuteReader();
+                    using(var tabla = new DataTable())
+                    {
+                        tabla.Load(reader);
+                        reader.Dispose();
+                        return tabla;
+                    }
+                }
+            }
+        }
+
+        protected DataTable ExecuteReaderParam(string transactSql)
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.Text;
+                    foreach (SqlParameter item in parametros)
+                    {
+                        comando.Parameters.Add(item);
+                    }
+                    SqlDataReader reader = comando.ExecuteReader();
+                    using (var tabla = new DataTable())
+                    {
+                        tabla.Load(reader);
+                        reader.Dispose();
+                        return tabla;
+                    }
+                }
+            }
+        }
+    }
+}
